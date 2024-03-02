2f0dcedab01d3d83c9f0ef78ed070befcc181eb7
jonas0214 <jonas0214@gmail.com>
2024-03-02 16:15:38 -0500

    En esta clase revisamos la funcion PHPdocs para documentar

---
+++ Clase_1/phpdoc.php
@@ -0,0 +1,17 @@
+<?php  
+
+
+/**
+ * Esta funcion retorna la suma de dos numeros
+ *
+ * @param int $n1
+ * @param int $n2
+ * @return int
+ */
+function suma($n1, $n2){
+    return $n1 + $n2 ;
+}
+
+$resultado = suma(1, 2);
+echo $resultado;
+echo "\n";
