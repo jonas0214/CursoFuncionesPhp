c654f9c518bdf8666de2828c2e2fdefe09f79ffa
jonas0214 <jonas0214@gmail.com>
2024-03-04 22:55:36 -0500

    Uso de mas funciones de fecha

--- Clase_3/date/funciones-fechas.php
+++ Clase_3/date/funciones-fechas.php
@@ -9,4 +9,11 @@ date_default_timezone_set("America/Mexico_City");
 //$now = date("Y-m-d H:i:s:a");
 //$now  = date_create(); // para fechas que se quisieran manipular- devuelve una clase dateTime
 
-echo strtotime("14 February 1996");
+//echo strtotime("14 February 1996");
+
+// diferencia de tiempo
+
+$today = new DateTime();
+$mrmochi_birth = new DateTime("2020-03-25");
+$interval = $mrmochi_birth->diff($today);
+echo $interval->format("%y años con %d dias");
